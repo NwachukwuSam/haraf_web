278c7c12752baf589e1f6cd4d9cb609e6ef05dcf
Nwachukwu Samuel <nwachukwusamuel123@gmail.com>
2026-04-20 17:21:23 +0100

    image corrections

--- src/components/home/LatestNews.jsx
+++ src/components/home/LatestNews.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import anniversaryImage from '../../assets/haraf.JPG';
-import maryamStoryImage from '../../assets/delivery.JPEG';
+import maryamStoryImage from '../../assets/delivery.jpeg';
 import healthEducationImage from '../../assets/announcementImage.jpg';
 
 // ──────────────────────────────────────────────────────────────────
